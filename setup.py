da71b02313869520274e3d91a6c49530aff1f0b5
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-03 13:58:55 +0000

    Version bump.
    git-svn-id: http://trac-hacks.org/svn/masterticketsplugin/0.11@3941 7322e99d-02ea-0310-aa39-e9a107903beb

--- setup.py
+++ setup.py
@@ -5,21 +5,33 @@ from setuptools import setup
 
 setup(
     name = 'TracMasterTickets',
-    version = '2.1.1',
+    version = '2.1.2',
     packages = ['mastertickets'],
     package_data = { 'mastertickets': ['templates/*.html', 'htdocs/*.js', 'htdocs/*.css' ] },
 
-    author = "Noah Kantrowitz",
-    author_email = "noah@coderanger.net",
-    description = "Provides support for ticket dependencies and master tickets.",
-    license = "BSD",
-    keywords = "trac plugin ticket dependencies master",
-    url = "http://trac-hacks.org/wiki/MasterTicketsPlugin",
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Provides support for ticket dependencies and master tickets.',
+    license = 'BSD',
+    keywords = 'trac plugin ticket dependencies master',
+    url = 'http://trac-hacks.org/wiki/MasterTicketsPlugin',
     classifiers = [
         'Framework :: Trac',
+        #'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
     ],
     
-    install_requires = ['Trac', 'Genshi >= 0.5.dev-r698,==dev'],
+    install_requires = ['Trac>=0.11', 'Genshi>=0.5'],
 
     entry_points = {
         'trac.plugins': [
