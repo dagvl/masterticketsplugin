aa171a0c7b423caf9417a2d88096965a08d20284
Dag Viggo Lokøen <dvl@vizrt.com>
2008-12-10 15:12:39 +0100

    Concurrent edits now blocked.
    This fixes the data-loss were two persons changes the blocking/blocked
    by fields concurrently and one set of changes is lost.

--- mastertickets/model.py
+++ mastertickets/model.py
@@ -75,6 +75,10 @@ class TicketLinks(object):
                            
                     cursor.execute('UPDATE ticket_custom SET value=%s WHERE ticket=%s AND name=%s',
                                    (new_value, n, field))
+
+                    # refresh the changetime to prevent concurrent edits
+                    cursor.execute('UPDATE ticket SET changetime=%s WHERE id=%s', (when_ts,n))
+
                     if not cursor.rowcount:
                         cursor.execute('INSERT INTO ticket_custom (ticket, name, value) VALUES (%s, %s, %s)',
                                        (n, field, new_value))
